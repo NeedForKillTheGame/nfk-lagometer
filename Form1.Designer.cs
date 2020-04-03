8a64232e56d8353c204160c88352f17c22ab2e17
HarpyWar <harpywar@gmail.com>
2020-04-03 17:19:13 +0300

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace NFKLagometer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbServerList = new System.Windows.Forms.ComboBox();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnReload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(1, 149);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Range;
+            series1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series1.Legend = "Legend1";
+            series1.Name = "Ping";
+            series1.YValuesPerPoint = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Average";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Count";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Lost";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(712, 184);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.Chart1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(278, 103);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(82, 25);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Server IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Server Port";
+            // 
+            // cmbServerList
+            // 
+            this.cmbServerList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbServerList.FormattingEnabled = true;
+            this.cmbServerList.Location = new System.Drawing.Point(24, 43);
+            this.cmbServerList.Name = "cmbServerList";
+            this.cmbServerList.Size = new System.Drawing.Size(248, 24);
+            this.cmbServerList.TabIndex = 6;
+            this.cmbServerList.SelectedIndexChanged += new System.EventHandler(this.CmbServerList_SelectedIndexChanged);
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(24, 106);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(162, 22);
+            this.txtIp.TabIndex = 2;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(195, 106);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(77, 22);
+            this.txtPort.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Server List";
+            // 
+            // btnReload
+            // 
+            this.btnReload.Location = new System.Drawing.Point(278, 43);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(82, 25);
+            this.btnReload.TabIndex = 1;
+            this.btnReload.Text = "Reload";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.BtnReload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 332);
+            this.Controls.Add(this.cmbServerList);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.chart1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "NFK Lagometer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbServerList;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnReload;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.Web.Script.Serialization;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace NFKLagometer
+{
+    public partial class Form1 : Form
+    {
+        private static List<PingItem> pings = new List<PingItem>();
+        private Random rnd = new Random();
+        StripLine stripline = new StripLine();
+        private int Interval = 100;
+        private int Timeout = 300;
+
+        private ushort ServerPort;
+        private IPAddress ServerIP;
+
+        private long PingCount = 0;
+        private long PingSum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        Dictionary<string, int> tags = new Dictionary<string, int>() {
+            { "test", 10 },
+            { "my", 3 },
+            { "code", 8 }
+        };
+
+        int lostCount = 0;
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            bool lost = false;
+            var lat = SendPing(ServerIP, ServerPort, ref lost);
+
+            if (lost)
+            {
+                lostCount++;
+                return;
+            }
+
+            var ping = new PingItem(lat);
+            //pings.Add(ping);
+            PingCount++;
+            PingSum += lat;
+
+            var avgPing = PingSum / PingCount;
+            stripline.IntervalOffset = avgPing;
+            chart1.Series[0].Name = "Ping  " + ping.GetPing();
+            chart1.Series[1].Name = "Average  " + (int)avgPing;
+            chart1.Series[2].Name = "Count  " + (int)PingCount;
+            chart1.Series[3].Name = "Lost  " + (int)lostCount;
+
+            chart1.Series[0].Points.AddXY(ping.GetTime(), ping.Ping);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = Interval;
+
+            BtnReload_Click(null, null);
+
+            // add averige line
+            stripline.Interval = 0;
+            stripline.StripWidth = 1;
+            stripline.BackColor = Color.DarkOrange;
+            chart1.ChartAreas[0].AxisY.StripLines.Add(stripline);
+
+            // set transpacency
+            chart1.Series[0].Color = Color.FromArgb(180, Color.Green);
+            chart1.Series[1].Color = stripline.BackColor;
+            chart1.Series[2].Color = Color.Transparent; // count
+            chart1.Series[3].Color = Color.Transparent; // lost
+        }
+
+
+
+
+        private static object DeserializeJson<T>(string Json)
+        {
+            var JavaScriptSerializer = new JavaScriptSerializer();
+            return JavaScriptSerializer.Deserialize<T>(Json);
+        }
+
+
+        bool started = false;
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if (!started)
+            {
+                // port as int
+                ushort.TryParse(txtPort.Text, out ServerPort);
+                txtPort.Text = ServerPort.ToString();
+                IPAddress.TryParse(txtIp.Text, out ServerIP);
+                if (ServerIP == null || ServerPort <= 0)
+                {
+                    MessageBox.Show("Invalid IP or Port", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                txtIp.Text = ServerIP.ToString();
+
+                btnStart.Text = "Stop";
+                btnReload.Enabled =
+                    txtIp.Enabled =
+                    txtPort.Enabled =
+                    cmbServerList.Enabled = false;
+
+                // reset
+                pings.Clear();
+                chart1.Series[0].Points.Clear();
+                PingCount = 0;
+                PingSum = 0;
+
+                timer1.Enabled = true;
+            }
+            else
+            {
+                btnStart.Text = "Start";
+                timer1.Enabled = false;
+
+                btnStart.Enabled =
+                    btnReload.Enabled =
+                    txtIp.Enabled =
+                    txtPort.Enabled =
+                    cmbServerList.Enabled = true;
+            }
+
+            // toggle
+            started = !started;
+        }
+
+
+        byte[] pingPacket = { 0x00, 0x01, 0x05, 0x01, 0x48 };
+        Stopwatch sw = new Stopwatch();
+
+        private int SendPing(IPAddress ip, ushort port, ref bool lost)
+        {
+            try
+            {
+                using (var client = new UdpClient(port))
+                {
+                    client.Client.SendTimeout = Timeout;
+                    client.Client.ReceiveTimeout = Timeout;
+                    var server = new IPEndPoint(ip, port);
+                    sw.Restart();
+                    client.Send(pingPacket, pingPacket.Length, server);
+                    byte[] packet = client.Receive(ref server);
+                    sw.Stop();
+                }
+            }
+            catch (Exception e)
+            {
+                lost = true;
+            }
+            return (int)sw.ElapsedMilliseconds;
+        }
+
+        private void BtnReload_Click(object sender, EventArgs e)
+        {
+            btnReload.Enabled = false;
+
+            var url = "https://stats.needforkill.ru/api.php?action=gsl";
+            using (var wc = new WebClient())
+            {
+                try
+                {
+                    var data = wc.DownloadString(url);
+                    var servers = DeserializeJson<List<ServerItem>>(data) as List<ServerItem>;
+                    cmbServerList.Items.Clear();
+                    foreach (var s in servers)
+                    {
+                        cmbServerList.Items.Add(s);
+                    }
+                    cmbServerList.SelectedIndex = 0;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error load servers from\n" + url, "API Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+
+            btnReload.Enabled = true;
+        }
+
+        private void CmbServerList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var item = (ServerItem)cmbServerList.SelectedItem;
+            var address = item.Ip.Split(':');
+            txtIp.Text = address[0];
+            txtPort.Text = address[1];
+        }
+
+        private void Chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.Width < 500)
+                this.Width = 500;
+            if (this.Height < 400)
+                this.Height = 400;
+        }
+    }
+
+    internal class ServerItem
+    {
+
+        public string Name;
+        public string Hostname;
+        public string Map;
+        public string Gametype;
+        public string Load;
+        public string Ip;
+        //public string[] Players;
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+
+    internal class PingItem
+    {
+        public PingItem(int ping)
+        {
+            Ping = ping;
+        }
+
+        public DateTime Date = DateTime.Now;
+        public int Ping = 0;
+
+        public string GetTime()
+        {
+            return Date.Hour + ":" + Date.Minute + ":" + Date.Second;
+        }
+
+        public string GetPing()
+        {
+            return Ping.ToString().PadLeft(3, ' ');
+        }
+    }
+}
